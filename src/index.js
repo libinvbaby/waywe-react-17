3808c325d084ca4d7f74e150afb616ad111d5d04
libin v Baby <82673070+libinvbaby@users.noreply.github.com>
2023-04-18 10:25:11 +0530

    updated

--- src/index.js
+++ src/index.js
@@ -4,5 +4,5 @@ const rootElement = document.getElementById("root");
 if (rootElement.hasChildNodes()) {
   hydrate(<App />, rootElement);
 } else {
-  render(<App />, rootElement);
+  render(<App />, rootElement)
 }
